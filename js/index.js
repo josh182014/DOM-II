a3a5911a6e614d29e0af7fb8f4d6d9d4da9f8624
josh182014 <josh182014@gmail.com>
2019-04-09 13:33:37 -0600

    organized files and added 3 events to index

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+//Fun in the Sun button alert
+document.getElementById("funSignUp").addEventListener("click", function(event){
+    alert("Fun In The Sun is currently full! Please try again at a later date!")
+});
+
+//Mountain Excursion mouseover and mouse leave events
+let mtnSignUpBtn = document.getElementById("mtnSignUp").addEventListener("mouseover", function(event){
+    event.target.innerText = "Pick Me!";
+  });
+document.getElementById("mtnSignUp").addEventListener("mouseleave", function(event){
+    event.target.innerText = "ok bye then"
+  });
+
+  //Island Getaway mouseleave event
+let islandSignUpBtn = document.getElementById("islandSignUp").addEventListener("mouseleave", function(event){
+    event.target.innerText = "Wait! Come back!"
+  })
+
+  
